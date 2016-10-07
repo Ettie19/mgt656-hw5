20486a67d95b580612ea77aec9e328a194e31f14
Ettie Nikolova <ettie.nikolova@yale.edu>
2016-10-06 23:16:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
-app.get('/thoughtful-cheetah', function(request, response){
-    response.send('Hello MGT-656!');
+app.get('/nickname', function(request, response){
+    response.send('thoughtful-cheetah');
 });
 
 app.listen(process.env.PORT || 4000);
